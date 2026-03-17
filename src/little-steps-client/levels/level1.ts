64efe7b57a4aa03b7d01c7b6c3816fc0c3902f21
Meng <meng81593@gmail.com>
2026-03-18 02:46:25 +0800

    feat: implement tilemap rendering and path-based movement and change dog'

--- src/little-steps-client/levels/level1.ts
+++ src/little-steps-client/levels/level1.ts
@@ -9,50 +9,114 @@ export const level1: LevelData = {
     maxResources: { stamina: 100 },
     nodes: [
         {
-            id: 0, x: 120, y: 320,
+            id: 0, x: 160, y: 390,
             type: NodeType.START,
             neighbors: [
-                { targetId: 1, effects: [{ resource: 'time', op: 'add', value: -10 }, { resource: 'stamina', op: 'add', value: -5 }] },
-                { targetId: 2, effects: [{ resource: 'time', op: 'add', value: -15 }, { resource: 'stamina', op: 'add', value: -10 }] }
+                { 
+                    targetId: 1, 
+                    effects: [{ resource: 'time', op: 'add', value: -10 }, { resource: 'stamina', op: 'add', value: -5 }],
+                    pathNodes: [
+                        { x: 160, y: 390 }, // 第1个点：必须是起点圆圈的中心
+                        { x: 230, y: 390 }, // 第2个点：石子路拐弯的地方（比如先向右走到 x:280）
+                        { x: 280, y: 190 }  // 第3个点：必须是终点(节点1)圆圈的中心
+                    ]
+                },
+                { 
+                    targetId: 2, 
+                    effects: [{ resource: 'time', op: 'add', value: -15 }, { resource: 'stamina', op: 'add', value: -10 }],
+                    pathNodes: [
+                        { x: 160, y: 410 },
+                        { x: 160, y: 490 }, // 假设先往下走
+                        { x: 430, y: 490 }  // 再往右走到节点2
+                    ]
+                }
             ]
         },
         {
-            id: 1, x: 330, y: 160,
+            id: 1, x: 280, y: 190,
             type: NodeType.PLAYER,
             neighbors: [
-                { targetId: 3, effects: [{ resource: 'time', op: 'add', value: -20 }, { resource: 'stamina', op: 'add', value: -10 }] }
+                { 
+                    targetId: 3, effects: [{ resource: 'time', op: 'add', value: -20 }, { resource: 'stamina', op: 'add', value: -10 }],
+                    pathNodes: [
+                        { x: 160, y: 190 }, 
+                        { x: 590, y: 190 }, 
+                        { x: 590, y: 310 }  
+                    ]
+                
+                }
             ],
+        
             nodeEffects: [
                 { resource: 'time', op: 'add', value: -5 },
                 { resource: 'stamina', op: 'add', value: 30 } 
             ]
         },
         {
-            id: 2, x: 330, y: 480,
+            id: 2, x: 430, y: 480,
             type: NodeType.DEFENDER,
             neighbors: [
-                { targetId: 3, effects: [{ resource: 'time', op: 'add', value: -10 }, { resource: 'stamina', op: 'add', value: -5 }] },
-                { targetId: 4, effects: [{ resource: 'time', op: 'add', value: -25 }, { resource: 'stamina', op: 'add', value: -15 }] }
+                { 
+                    targetId: 3, effects: [{ resource: 'time', op: 'add', value: -10 }, { resource: 'stamina', op: 'add', value: -5 }],
+                    pathNodes: [
+                        { x: 430, y: 480 }, 
+                        { x: 430, y: 460 },
+                        { x: 590, y: 460 }, 
+                        { x: 590, y: 310 } 
+                    ] 
+                },
+                { 
+                    targetId: 4, effects: [{ resource: 'time', op: 'add', value: -25 }, { resource: 'stamina', op: 'add', value: -15 }],
+                    pathNodes: [
+                        { x: 430, y: 480 }, 
+                        { x: 430, y: 460 }, 
+                        { x: 750, y: 460 }, 
+                        { x: 750, y: 520 } 
+                    ] 
+                }
+            
             ]
         },
         {
-            id: 3, x: 560, y: 280,
+            id: 3, x: 590, y: 310,
             type: NodeType.PLAYER,
             neighbors: [
-                { targetId: 5, effects: [{ resource: 'time', op: 'add', value: -15 }, { resource: 'stamina', op: 'add', value: -5 }] }
+                { 
+                    targetId: 5, effects: [{ resource: 'time', op: 'add', value: -15 }, { resource: 'stamina', op: 'add', value: -5 }],
+                    pathNodes: [
+                        { x: 590, y: 310 }, 
+                        { x: 850, y: 310 }, 
+                        { x: 850, y: 380 } 
+                    ] 
+            }
             ],
             nodeEffects: [{ resource: 'bones', op: 'add', value: 1 }] 
         },
         {
-            id: 4, x: 560, y: 520,
+            id: 4, x: 750, y: 520,
             type: NodeType.PLAYER,
             neighbors: [
-                { targetId: 5, effects: [{ resource: 'time', op: 'add', value: -5 }, { resource: 'stamina', op: 'add', value: -5 }] },
-                { targetId: 2, effects: [{ resource: 'time', op: 'add', value: -5 }, { resource: 'stamina', op: 'add', value: 50 }] }
+                { 
+                    targetId: 5, effects: [{ resource: 'time', op: 'add', value: -5 }, { resource: 'stamina', op: 'add', value: -5 }],
+                    pathNodes: [
+                        { x: 750, y: 520 }, 
+                        { x: 850, y: 520 }, 
+                        { x: 850, y: 380 } 
+                    ] 
+            },
+                { 
+                    targetId: 2, effects: [{ resource: 'time', op: 'add', value: -5 }, { resource: 'stamina', op: 'add', value: 50 }],
+                    pathNodes: [
+                        { x: 750, y: 520 }, 
+                        { x: 750, y: 540 }, 
+                        { x: 430, y: 540 }, 
+                        { x: 430, y: 480 } 
+                    ] 
+                }
             ]
         },
         { 
-            id: 5, x: 810, y: 320, 
+            id: 5, x: 850, y: 380, 
             type: NodeType.GOAL,
             neighbors: [] 
         }
